bdcdf370587ebaa74f38ac675dc52ef6153f1cf4
subaru-hello <octpsubaru@icloud.com>
2021-06-16 09:24:51 +0900

    Validation done

--- db/migrate/20210615143533_create_tasks.rb
+++ db/migrate/20210615143533_create_tasks.rb
@@ -1,9 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, limot: 30, null: false
       t.text :description
-
       t.timestamps
     end
   end
