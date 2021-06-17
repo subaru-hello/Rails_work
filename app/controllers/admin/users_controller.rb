a70a7eacec2efac6ac0543f4f7b1658d29bb9426
subaru-hello <octpsubaru@icloud.com>
2021-06-17 21:57:06 +0900

    現実の複雑sない対応する #1

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,7 +19,7 @@ def create
 @user = User.new(user_params)
 
 if @user.save
-  redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を登録しました。"
+  redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
 else
   render :new
 end
@@ -30,7 +30,7 @@ def update
   @user = User.find(params[:id])
 
   if @user.update(user_params)
-    redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を更新しました。"
+    redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
   else
     render :edit
   end
@@ -39,7 +39,7 @@ end
 def destroy
   @user = User.find(params[:id])
   @user.destroy
-  redirect_to admin_user_url, notice: "ユーザー「#{user.name}」を更新しました。"
+  redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を更新しました。"
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   get '/login',to: 'sessions#new'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
   end
