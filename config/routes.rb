6cbe8eb57ac44deeb938620a2c6952bd375c7fb6
subaru-hello <octpsubaru@icloud.com>
2021-06-17 09:08:07 +0900

    Login add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get '/login',to: 'sessions#new'
+  post '/login', to: 'session#create'
+  namespace :admin do
+    resources :users
+  end
 resources :tasks
 root to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
