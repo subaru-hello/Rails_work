fc34f947f385357d2eda1189bb3dd7cf50ffbd15
subaru-hello <octpsubaru@icloud.com>
2021-06-21 10:23:45 +0900

    CSV形式のファイルのインポートエクスポートを行う #16

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,6 +56,12 @@ def confirm_new
   render :new unless @task.valid?
 end
 
+
+def import
+  current_user.tasks.import(params[:file])
+  redirect_to tasks_url, notice: "タスクを追加しました"
+end
+
   private
 
   def task_params
